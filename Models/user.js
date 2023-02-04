36eb9040a652a18233f4437fd93b3119ff3c339b
ryudg <ryudg95@gmail.com>
2023-02-04 20:58:47 +0900

    One to Few Relationship
    사용자가 두가지나 그 이상의 여러가지 항목을 가질때 관계를 저장하는 방법

--- Models/user.js
+++ Models/user.js
@@ -10,3 +10,52 @@ mongoose
     console.log("ERROR");
     console.log(err);
   });
+
+// 유저 스키마 정의
+const userSchema = new mongoose.Schema({
+  first: String,
+  last: String,
+  addresses: [
+    {
+      _id: { _id: false }, // mongoose _id 생성 막기
+      street: String,
+      city: String,
+      state: String,
+      country: String,
+    },
+  ],
+});
+
+const User = mongoose.model("User", userSchema);
+
+// 유저 생성
+const makeUser = async () => {
+  const u = new User({
+    first: "Harry",
+    last: "Potter",
+  });
+  u.addresses.push({
+    street: "123 Sesame St.",
+    city: "New York",
+    state: "NY",
+    country: "USA",
+  });
+  const res = await u.save();
+  console.log(res);
+};
+
+// 새 주소 추가
+const addAdress = async (id) => {
+  const user = await User.findById(id);
+  user.addresses.push({
+    street: "99 3rd St.",
+    city: "New York",
+    state: "NY",
+    country: "USA",
+  });
+  const res = await user.save();
+  console.log(res);
+};
+
+// makeUser();
+addAdress("63de46fd13cdf0df1499bfab");
